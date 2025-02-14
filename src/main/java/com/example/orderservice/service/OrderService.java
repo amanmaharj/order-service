d02a711821f84672ada14a5c7bf75bf6b74fdd61
amanmaharj <aman.mahaj@gmail.com>
2025-02-14 18:21:18 -0500

    Introduced the interprocess communication between inventory service and order service through synchronous communication with feignclient, updated the name of the DB name in inventory service.

--- src/main/java/com/example/orderservice/service/OrderService.java
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.orderservice.service;
 
+import com.example.orderservice.client.InventoryClient;
 import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.repository.OrderRepository;
@@ -13,14 +14,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     public void placeOrder(OrderRequest orderRequest){
-        Order order = Order.builder().orderNumber(UUID.randomUUID().toString())
-                .price(orderRequest.price())
-                .skuCode(orderRequest.skuCode())
-                .price(orderRequest.price())
-                .quantity(orderRequest.quantity()).build();
+       var isInStock = inventoryClient.isInventoryInStock(orderRequest.skuCode(), orderRequest.quantity());
+       if(isInStock){
+           Order order = Order.builder().orderNumber(UUID.randomUUID().toString())
+                   .price(orderRequest.price())
+                   .skuCode(orderRequest.skuCode())
+                   .price(orderRequest.price())
+                   .quantity(orderRequest.quantity()).build();
 
-        orderRepository.save(order);
+           orderRepository.save(order);
+       } else {
+           throw new RuntimeException("product with SkuCode " + orderRequest.skuCode() + " is not in stock.");
+       }
+
 
     }
 
