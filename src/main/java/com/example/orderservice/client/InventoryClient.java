97aa6988688a9330c0a1e8bbf1865a70edff080b
amanmaharj <aman.mahaj@gmail.com>
2025-03-11 20:38:00 -0400

    modify the inner commucication between order and inventory service using web client

--- src/main/java/com/example/orderservice/client/InventoryClient.java
+++ src/main/java/com/example/orderservice/client/InventoryClient.java
@@ -2,22 +2,18 @@ package com.example.orderservice.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-
-@FeignClient(value = "inventory", url = "${inventory.url}")
 
 public interface InventoryClient {
 
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     @Retry(name = "inventory")
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInventoryInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
     default boolean fallbackMethod(String code, Integer quantity, Throwable throwable){
--- src/main/java/com/example/orderservice/service/OrderService.java
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -5,26 +5,31 @@ import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest){
        var isInStock = inventoryClient.isInventoryInStock(orderRequest.skuCode(), orderRequest.quantity());
        if(isInStock){
            Order order = Order.builder().orderNumber(UUID.randomUUID().toString())
                    .price(orderRequest.price())
                    .skuCode(orderRequest.skuCode())
-                   .price(orderRequest.price())
                    .quantity(orderRequest.quantity()).build();
 
            orderRepository.save(order);
+           //send the message to Kafka topic which we have to inclue the email address and message.
+
        } else {
            throw new RuntimeException("product with SkuCode " + orderRequest.skuCode() + " is not in stock.");
        }
